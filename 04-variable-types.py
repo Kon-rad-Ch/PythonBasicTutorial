2f7a13636374b8aafc364d50dab95b5988fca548
Kon-rad-Ch <konrad.ch.pl@gmail.com>
2021-10-17 01:53:59 +0200

    test multiple files commit

---
+++ 04-variable-types.py
@@ -0,0 +1,58 @@
+# --------strings------------------
+# name = "Konrad"
+# name = "Konrad's program"
+# name = 'Konrad\'s\n\n program'
+# name = """Konrad's
+
+#  program"""
+# print(name)
+
+# ---------/strings-----------------
+
+# ---------numbers-----------------
+
+# a = 10 #int
+# b = 2.5   #float
+
+# print(a +b)  #automatic type conversion and calculation
+
+# a = 100
+# a = 1000
+# a = 10000
+# a = 1_000_000  # just to make human reading easier
+
+# print(a)
+
+# ---------/numbers-----------------
+
+# ---------boolean-----------------
+
+# is_sky_blue = True   # convention called snake_case
+# is_sun_blue = False   # convention from java called camelCase
+
+# print("Is it blue?")
+# print("Sky:")  
+# print(is_sky_blue)
+# print("Sun:")  
+# print(is_sun_blue)
+
+# ---------/boolean-----------------
+
+# ---------type check-----------------
+
+# print(type(name))
+# print(type(a))
+# print(type(is_sky_blue))
+
+
+# ---------/type check-----------------
+
+# ---------class convertion-----------------
+
+first = 2
+second = "3"
+
+print(first + int(second))
+print(str(first) + second)
+print(str(first) + str(second))
+# ---------/class convertion-----------------
---
+++ 05-math-operators.py
@@ -0,0 +1,25 @@
+a = 4
+b = 2
+# print(a + b)
+# print(type(a + b))
+# print(a - b)
+# print(a * b)
+# print(a / b)  #In python3 auto conversion to float. In python2 integer (rounding to lower)
+# print(type(a / b))
+
+# a=4.0
+# print(a * b)
+# print(type(a + b)) # float, because one var was float
+# print(a / b)
+# print(type(a / b))
+
+
+# print(a % b)   # modulo
+# print(a ** b)  # a raised to the power of b
+
+
+k = 5
+
+# k = k + 1
+k += 1 # also -= , *= /=
+print(k)
